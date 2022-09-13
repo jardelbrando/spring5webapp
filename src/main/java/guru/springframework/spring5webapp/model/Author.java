5a3c07d2308212646d686f1e1ed400fd60ad6e3e
jardelbrando <jardelbrando@gmail.com>
2022-09-13 00:21:34 -0300

    Assigment dos authors feito

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -55,7 +55,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
